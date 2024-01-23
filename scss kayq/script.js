c26f1ad246ea16428da6844cd5f8c9dfc344452b
BenoSahakyan2007 <157481629+BenoSahakyan2007@users.noreply.github.com>
2024-01-23 14:47:59 +0400

    Add files via upload

---
+++ scss kayq/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.tab-list').on('click', '.tab', function(){
+        $('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('.tab-content').removeClass('show');
+        $($(this).attr('href')).addClass('show');
+    })
+})
